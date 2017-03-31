4a64946083de9203c8edf6ff489aa85c95e3354d
eadpearce <e.pearce@tutanota.com>
2017-03-31 16:50:51 +0100

    Finished basic game logic

--- js/app.js
+++ js/app.js
@@ -1,35 +1,76 @@
 // piano samples edited from those provided here http://theremin.music.uiowa.edu/MISpiano.html
+const pcNotes = {
+  0: null,
+  1: null,
+  2: null
+};
+const playerNotes = {
+  0: null,
+  1: null,
+  2: null
+};
+const allKeys = [0,1,2,3,4,5,6,7,8,9,10,11];
+// const blackKeys = [1,3,6,8,10];
+const whiteKeys = [0,2,4,5,7,9,11];
+let time = 0;
+function genRand(array) {
+  const randIndex = Math.floor(Math.random() * array.length);
+  return randIndex;
+}
 
 $( () => {
-
+  const $start = $('.start');
   const $keys = $('.keys');
-  const allKeys = [0,1,2,3,4,5,6,7,8,9,10,11];
-  const sounds = ['00-c','01-db','02-d', '03-eb', '04-e', '05-f', '06-gb', '07-g', '08-ab', '09-a', '10-bb', '11-b'];
-  const blackKeys = [1,3,6,8,10];
-  const whiteKeys = [0,2,4,5,7,9,11];
-  const audio = document.getElementById('test');
+  const audio = document.getElementById('piano');
+  let canPlay = false;
 
-  $keys.on('mousedown', function(e) {
-    console.log(e.target.id);
+  function checkMatch() {
+    if (playerNotes[0] === pcNotes[0] && playerNotes[1] === pcNotes[1] && playerNotes[2] === pcNotes[2]) {
+      console.log('correct! yuo are smart');
+
+      pcNotes[0] = null;
+      pcNotes[1] = null;
+      pcNotes[2] = null;
+    }
+  }
+
+
+  // piano playback
+  $keys.on('click', function(e) {
+    // console.log(e.target.id);
     audio.src = 'sounds/' + e.target.id +'.ogg';
     audio.play();
-    // this.src = 'sounds/' + $audio[this.id].id + '.ogg';
-    // $audio[this.id].play();
+
+    if (canPlay) {
+      playerNotes[time] = parseInt(e.target.id);
+      time++;
+      console.log(playerNotes);
+      if (time===3) {
+        console.log('finish');
+        // check if match
+        checkMatch();
+      }
+    }
   });
 
-  // const $audio = $('audio');
+  // need button to start playback of notes
+  $start.on('click', function() {
+    pcNotes[0] = whiteKeys[genRand(whiteKeys)];
+    pcNotes[1] = whiteKeys[genRand(whiteKeys)];
+    pcNotes[2] = whiteKeys[genRand(whiteKeys)];
+    console.log(pcNotes);
+    const timer = setInterval( () => {
+      // console.log(timeRemaining);
+      audio.src = 'sounds/' + pcNotes[time] + '.ogg';
+      audio.play();
+      time++;
+      if (time > 2) {
+        clearInterval(timer);
+        time = 0;
+        canPlay = true;
+      }
+    }, 1000);
+  });
 
-  //
-  // for (let i=0; i<16; i++) {
-  //   button[i].addEventListener('mousedown', () => {
-  //     // audio.src = 'sounds/' + sounds[i] + '.wav';
-  //     audio.src = 'sounds/' + audio[i].id + '.wav';
-  //     audio[i].play();
-  //     console.log(sounds[i]);
-  //   });
-  // }
-
-
-  console.log('it works');
 
 });
