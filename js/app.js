bf6c93bc1ad0d32633b5535a69fbc457888dc88a
eadpearce <e.pearce@tutanota.com>
2017-04-02 14:57:52 +0100

    Put level data into one big object. Added level dropdown and difficulty options.

--- js/app.js
+++ js/app.js
@@ -4,48 +4,23 @@
 // const blacknotes = [1,3,6,8,10];
 // const whitenotes = [0,2,4,5,7,9,11];
 const keyboardControl = [65, 87, 83, 69, 68, 70, 84, 71, 89, 72, 85, 74];
-// level1 uses only 3 of the white notes and a phrase length of 3
-// level is 3 turns long
-const level1 = {
-  notes: [0,2,4],
-  phraseLength: 3
+const levels = {
+  1: { notes: [0,2,4], phraseLength: 3 },
+  2: { notes: [0,2,4,5,7], phraseLength: 4 },
+  3: { notes: [0,2,4,5,7,9,11], phraseLength: 4 },
+  4: { notes: [0,2,4,5,7,9,11], phraseLength: 5 },
+  5: { notes: [0,1,2,3,4,5,6,7], phraseLength: 5 },
+  6: { notes: [0,1,2,3,4,5,6,7,8,9,10,11], phraseLength: 5 },
+  7: { notes: [0,1,2,3,4,5,6,7,8,9,10,11], phraseLength: 5 }
 };
-// level2 uses 5 whites notes
-const level2 = {
-  notes: [0,2,4,5,7],
-  phraseLength: 5
-};
-// level3 uses all the white notes
-const level3 = {
-  notes: [0,2,4,5,7,9,11],
-  phraseLength: 5
-};
-// uses all white notes and phrase length of 4
-const level4 = {
-  notes: [0,2,4,5,7,9,11],
-  phraseLength: 5
-};
-// uses black and white notes up to E, phrase length of 4
-const level5 = {
-  notes: [0,1,2,3,4],
-  phraseLength: 3
-};
-// uses all notes
-const level6 = {
-  notes: [0,1,2,3,4,5,6,7,8,9,10,11],
-  phraseLength: 5
-};
-// uses all notes, phrase length of 5
-const level7 = {
-  notes: [0,1,2,3,4,5,6,7,8,9,10,11],
-  phraseLength: 5
-};
+
 // start at level1 by default
-let currentLevel = level1;
+let currentLevel = levels[1];
 // time to keep track of notes played
 let time = 0;
 let playerNotes = [];
 let pcNotes = [];
+let currentDifficulty = 'easy';
 
 
 function genRand(length) {
@@ -63,8 +38,9 @@ function genRand(length) {
 }
 
 $( () => {
-  const $start = $('.start');
+  const $play = $('.play');
   const $keys = $('.keys');
+  const $winmsg = $('.winmsg');
 
   // set up audio tags and src
   const container = document.querySelector('.container');
@@ -74,7 +50,6 @@ $( () => {
     audios.src = 'sounds/audio'+i+'.ogg';
     container.appendChild(audios);
   }
-
   const $audio = $('audio');
   let canPlay = false;
 
@@ -84,19 +59,40 @@ $( () => {
     for (let i = 0; i < pcNotes.length; i++) {
       if (pcNotes[i] !== playerNotes[i]) {
         playerNotes = [];
+        $winmsg.html('Try again...');
+        setTimeout( () => {
+          $winmsg.html('');
+        }, 1000);
         return false;
       }
     }
     // reset the array
     playerNotes = [];
+    $winmsg.html('Correct!');
+    setTimeout( () => {
+      $winmsg.html('');
+    }, 1000);
     return true;
   }
+
+  $('select').on('change', function() {
+    currentLevel = levels[parseInt(this.value.slice(5))];
+  });
+
+  $('.difficulty').on('change', function(e) {
+    if (e.target.value === 'easy') {
+      currentDifficulty = 'easy';
+    } else if (e.target.value === 'hard') {
+      currentDifficulty = 'hard';
+    }
+  });
 
   $(document).keydown( function(e) {
     if ($.inArray( e.keyCode, keyboardControl)===-1) {
       return;
     }
     const keyboardNote = keyboardControl.indexOf(e.keyCode);
+
     $('#key'+keyboardNote).addClass('depress');
     setTimeout( () => {
       $('#key'+keyboardNote).removeClass('depress');
@@ -125,9 +121,10 @@ $( () => {
     // console.log(keyId);
     $audio[keyId].currentTime=0;
     $audio[keyId].play();
-    $('#key'+keyId).addClass('depress');
+
+    $('#key'+thisKey).addClass('depress');
     setTimeout( () => {
-      $('#key'+keyId).removeClass('depress');
+      $('#key'+thisKey).removeClass('depress');
     }, 500);
 
     if (canPlay) {
@@ -146,8 +143,7 @@ $( () => {
   });
 
   // button to start playback of notes
-  // need way to make sure same note is not chosen twice
-  $start.on('click', function() {
+  $play.on('click', function() {
     playerNotes = [];
     pcNotes = [];
     pcNotes = genRand(currentLevel.phraseLength);
@@ -155,15 +151,17 @@ $( () => {
       const thisNote = pcNotes[time];
       // console.log(time);
       // console.log($('#key'+thisNote));
-      $('#key'+thisNote).addClass('depress');
-      setTimeout( () => {
-        $('#key'+thisNote).removeClass('depress');
-      }, 500);
+      if (currentDifficulty === 'easy') {
+        $('#key'+thisNote).addClass('depress');
+        setTimeout( () => {
+          $('#key'+thisNote).removeClass('depress');
+        }, 500);
+      }
       $audio[thisNote].currentTime=0;
       $audio[thisNote].play();
       time++;
       if (time === pcNotes.length) {
-        console.log('pcNotes: '+pcNotes);
+        // console.log('pcNotes: '+pcNotes);
         clearInterval(timer);
         time = 0;
         canPlay = true;
